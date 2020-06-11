902620fbabb574fd7d6572f6f828aa83602cdfc3
Eamonn Smyth <e.smyth75@gmail.com>
2020-06-11 09:46:21 +0000

    Commit Email functionalitySetup emails from form on contact page

---
+++ assets/js/sendEmail.js
@@ -0,0 +1,60 @@
+/* let contactForm = document.getElementById("contactForm");
+
+$("#contactForm").onsubmit(function() {
+    sendMail(contactForm);
+    contactForm.reset();
+});
+function sendMail(contactForm) {
+    emailjs.send("gmail", "rosie", {
+        "from_name": contactForm.name.value,
+        "from_email": contactForm.emailaddress.value,
+        "project_request": contactForm.projectsummary.value
+    })
+    .then(
+        function(response) {
+            console.log("SUCCESS", response);
+        },
+        function(error) {
+            console.log("FAILED", error);
+        }
+    ); 
+    return false;
+} */
+
+
+
+/* document.getElementById("submit").onclick = function(event){
+    event.preventDefault();
+
+    emailjs.send("gmail", "rosie", {
+        "from_name": contactForm.name.value,
+        "from_email": contactForm.emailaddress.value,
+        "project_request": contactForm.projectsummary.value
+    })
+    .then(
+        function(response) {
+            console.log("SUCCESS", response);
+        },
+        function(error) {
+            console.log("FAILED", error);
+        }
+    ); 
+    return false;
+}; */
+
+function sendMail(contactForm) {
+    emailjs.send("gmail", "rosie", {
+        "from_name": contactForm.name.value,
+        "from_email": contactForm.emailaddress.value,
+        "project_request": contactForm.projectsummary.value
+    })
+    .then(
+        function(response) {
+            console.log("SUCCESS", response);
+        },
+        function(error) {
+            console.log("FAILED", error);
+        }
+    );
+    return false;
+}
